6430b101461562951682f8c4373d68c0f56b4157
mprinceps <64213121+bcp2415@users.noreply.github.com>
2021-10-18 15:21:25 +0000

    adds constructors for class Manual and class Novel.

--- index.js
+++ index.js
@@ -8,11 +8,29 @@ class Book {
     this.numberPages = numberPages;
     this.numberTimesCheckedOut = numberTimesCheckedOut;
     this.isDiscarded = isDiscarded;
-  };
+  }
 }
 
 
 // Define your Manual and Novel classes here:
+class Manual extends Book {
+  constructor() {
+    super();
+  }
+  this.isTooOld = function(this.copyrightDate) {
+    const currentYear = 2021;
+    return (currentYear - copyrightDate) > 5;
+  }
+};
+
+class Novel extends Book {
+  constructor() {
+    super();
+  }
+  this.isReadyForRetirement(this.numberTimesCheckedOut) {
+    return (this.numberTimesCheckedOut > 100);
+  }
+};
 
 
 // Declare the objects for exercises 2 and 3 here:
