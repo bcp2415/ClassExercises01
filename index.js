889a37a89eb518563695cb14488c2017ef460eef
mprinceps <64213121+bcp2415@users.noreply.github.com>
2021-10-18 15:49:32 +0000

    completes homework exercises.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ class Manual extends Book {
 
   isTooOld(currentYear) {
     if ((currentYear - this.copyrightDate) > 5) {
-      this.isDiscarded = true;
+      this.isDiscarded = 'Yes';
     }
   }
 }
@@ -36,12 +36,18 @@ class Novel extends Book {
 
   isReadyForRetirement() {
     if (this.numberTimesCheckedOut > 100) {
-      this.isDiscarded = true;
+      this.isDiscarded = 'Yes';
     }
   }
 }
 
 // Declare the objects for exercises 2 and 3 here:
+let prideAndPrejudice = new Novel('Pride and Prejudice', 'Jane Austen', 1813, 1111111111111, 432, 32, 'No');
 
+let shuttleBuilding = new Manual('Top Secret Shuttle Building Manual', 'Redacted', 2013, 0000000000000, 1147, 1, 'No');
 
 // Code exercises 4 & 5 here:
+shuttleBuilding.isTooOld(2021);
+
+prideAndPrejudice.checkout(5);
+
