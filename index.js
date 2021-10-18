cda36b3d24062370e126e061aacfabd4e80488c3
mprinceps <64213121+bcp2415@users.noreply.github.com>
2021-10-18 11:33:19 +0000

    adds Book class & constructor function

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 // Define your Book class here:
+class Book {
+  constructor(title, author, copyrightDate, ISBN, numberPages, numberTimesCheckedOut, isDiscarded) {
+    this.title = title;
+    this.author = author;
+    this.copyrightDate = copyrightDate;
+    this.ISBN = ISBN;
+    this.numberPages = numberPages;
+    this.numberTimesCheckedOut = numberTimesCheckedOut;
+    this.isDiscarded = isDiscarded;
+  };
+}
 
 
 // Define your Manual and Novel classes here:
