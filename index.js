1e12725c10ee7e0da015ebfa4576d68198a0db83
mprinceps <64213121+bcp2415@users.noreply.github.com>
2021-10-18 15:40:54 +0000

    further fixes to syntax.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ class Book {
     this.numberPages = numberPages;
     this.numberTimesCheckedOut = numberTimesCheckedOut;
     this.isDiscarded = isDiscarded;
-  },
+  }
 
   checkout(uses = 1) {
     this.numberTimesCheckedOut += uses;
@@ -20,26 +20,26 @@ class Book {
 class Manual extends Book {
   constructor(title, author, copyrightDate, ISBN, numberPages, numberTimesCheckedOut, isDiscarded) {
     super(title, author, copyrightDate, ISBN, numberPages, numberTimesCheckedOut, isDiscarded);
-  },
+  }
 
   isTooOld(currentYear) {
     if ((currentYear - this.copyrightDate) > 5) {
       this.isDiscarded = true;
+    }
   }
-
-};
+}
 
 class Novel extends Book {
   constructor(title, author, copyrightDate, ISBN, numberPages, numberTimesCheckedOut, isDiscarded) {
     super(title, author, copyrightDate, ISBN, numberPages, numberTimesCheckedOut, isDiscarded);
-  },
+  }
 
-  isReadyForRetirement(this.numberTimesCheckedOut) {
+  isReadyForRetirement() {
     if (this.numberTimesCheckedOut > 100) {
       this.isDiscarded = true;
+    }
   }
-};
-
+}
 
 // Declare the objects for exercises 2 and 3 here:
 
